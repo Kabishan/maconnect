2573f0014c95269ca895a5bb5a729f7dfbdd2318
Kabishan <suvendrk@mcmaster.ca>
2020-05-19 18:48:33 -0400

    Connected Mongoose to MongoDB

---
+++ config/db.js
@@ -0,0 +1,25 @@
+/* Importing Mongoose and Global Values */
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+/* Connecting Mongoose to MongoDB */
+const connectDB = async () => {
+  try {
+    /* Added these parameters to resolve 
+       deprecated warnings */
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error(error.message);
+    /* Exit the process if something goes wrong */
+    process.exit(1);
+  }
+};
+
+/* Exporting this function to be made available 
+   to other modules */
+module.exports = connectDB;
