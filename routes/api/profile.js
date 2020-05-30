4219c978f29e1b3721fd42d59936f1b6f7e030e1
Kabishan <suvendrk@mcmaster.ca>
2020-05-30 19:19:54 -0400

    Main: Added github connection; can request to Github to obtain five most recent repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,8 @@ const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
 const { check, validationResult } = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 // @route    GET api/profile/me
 // @brief    Get logged in user's profile
@@ -390,4 +392,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+// @route    GET api/profile/github/:username
+// @brief    Get user repositories from Github
+// @access   Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `http://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(400).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
