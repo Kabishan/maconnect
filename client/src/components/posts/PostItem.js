b7d1a01e186ac3c31fd43a564b60da9c42e7169b
Kabishan <suvendrk@mcmaster.ca>
2020-06-11 18:15:02 -0400

    Added ability for other marauders to comment on posts

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { post } from 'request';
 import { addLike, removeLike, deletePost } from '../../actions/post';
 
 const PostItem = ({
+  showActions,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
   addLike,
@@ -26,39 +26,47 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
         </p>
-        <button
-          onClick={e => addLike(_id)}
-          type='button'
-          className='btn btn-light'
-        >
-          <i className='fa fa-thumbs-up' />{' '}
-          {likes.length > 0 && <span>{likes.length}</span>}
-        </button>
-        <button
-          onClick={e => removeLike(_id)}
-          type='button'
-          className='btn btn-light'
-        >
-          <i className='fa fa-thumbs-down' />
-        </button>
-        <Link to={`/post/${_id}`} className='btn btn-primary'>
-          Discussion{' '}
-          {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!auth.loading && auth.user._id === user && (
-          <button
-            onClick={e => deletePost(_id)}
-            type='button'
-            className='btn btn-danger'
-          >
-            <i className='fa fa-times' />
-          </button>
+        {showActions && (
+          <Fragment>
+            <button
+              onClick={e => addLike(_id)}
+              type='button'
+              className='btn btn-light'
+            >
+              <i className='fa fa-thumbs-up' />{' '}
+              {likes.length > 0 && <span>{likes.length}</span>}
+            </button>
+            <button
+              onClick={e => removeLike(_id)}
+              type='button'
+              className='btn btn-light'
+            >
+              <i className='fa fa-thumbs-down' />
+            </button>
+            <Link to={`/post/${_id}`} className='btn btn-primary'>
+              Discussion{' '}
+              {comments.length > 0 && (
+                <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && auth.user._id === user && (
+              <button
+                onClick={e => deletePost(_id)}
+                type='button'
+                className='btn btn-danger'
+              >
+                <i className='fa fa-times' />
+              </button>
+            )}
+          </Fragment>
         )}
       </div>
     </div>
   );
+};
+
+PostItem.defaultProps = {
+  showActions: true,
 };
 
 PostItem.propTypes = {
